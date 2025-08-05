5cdc0c3d8f2620ba1cd53efe95ef961d6c04a226
Kathryn Doering <kathryn.doering@noaa.gov>
2025-08-05 23:18:24 +0000

    refactor: build triggers separate out build trigger function, use in doc and style r, update_pkgdown

--- R/utils.R
+++ R/utils.R
@@ -70,3 +70,33 @@ add_quarto_true <- function(uses_line, gha) {
   gha <- append(gha, "      depends_on_quarto: true", after = uses_line + 1)
   gha
 }
+
+#' Add the appropriate build trigger to workflows.
+#' @param build_trigger Text of the build trigger option to add
+#' @param gha The workflow file that has been read in using readLines
+#' @return The modified workflow file
+#' @noRd
+add_build_trigger <- function(build_trigger, gha) {
+  build_trigger_lines <- switch(build_trigger,
+    push_to_main = c("  push:", "    branches: [main]"),
+    push_to_all_branches = c("  push:"),
+    pull_request = "  pull_request:",
+    pr_comment = c("  issue_comment:", "    types: [created] "),
+    manually = "  workflow_dispatch:",
+    weekly = c(
+      "  schedule:",
+      "# Use https://crontab.guru/ to edit the time",
+      "    - cron:  '15 02 * * 0'"
+    )
+  )
+
+  if (build_trigger == "pr_comment") {
+    job_name_line <- grep("call-workflow:", gha, fixed = TRUE)
+    if_statement <- c(
+      "    if: ${{ github.event.issue.pull_request && ",
+      "            (github.event.comment.author_association == 'MEMBER' || github.event.comment.author_association == 'OWNER') &&",
+      "            (startsWith(github.event.comment.body, '/doc-and-style')) }}"
+    )
+    gha <- append(gha, if_statement, after = job_name_line)
+  }
+}
